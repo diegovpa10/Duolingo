243fda98df46f4945701231927a26131d2ce6ba7
Diego Vargas <diegovpa10@gmail.com>
2026-03-23 13:55:54 -0600

    first commit

---
+++ aprendizaje/models.py
@@ -0,0 +1,164 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
+
+# --- 1. SISTEMA DE USUARIOS ---
+
+# La clase usuario creada a mano fue eliminado para usar la predefinida de Django
+
+class Estudiante(models.Model):
+    # ¡AQUÍ ESTÁ EL CAMBIO MÁGICO! Cambiamos 'Usuario' por 'User'
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    
+    racha_dias = models.IntegerField(default=0)
+    # Nota: Quité los validators temporalmente para simplificar, pero puedes dejarlos si ya los importaste
+    xp_total = models.BigIntegerField(default=0)
+
+    def __str__(self):
+        return f"Estudiante: {self.usuario.username}"
+
+class Reclutador(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    empresa = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"Reclutador: {self.empresa} ({self.usuario.username})"
+
+# --- 2. EMPLEABILIDAD ---
+
+class PerfilProfesional(models.Model):
+    estudiante = models.OneToOneField(Estudiante, on_delete=models.CASCADE)
+    biografia = models.TextField(blank=True, null=True)
+    url_github = models.URLField(max_length=500, blank=True, null=True)
+    url_linkedin = models.URLField(max_length=500, blank=True, null=True)
+    disponible = models.BooleanField(default=False)
+    # Django tiene un campo especial para JSON, ¡es perfecto para tu diseño!
+    habilidades = models.JSONField(blank=True, null=True) 
+
+    def __str__(self):
+        return f"Perfil de {self.estudiante.usuario.email}"
+
+class OfertaLaboral(models.Model):
+    reclutador = models.ForeignKey(Reclutador, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=200)
+    rango_salarial = models.CharField(max_length=100, blank=True, null=True)
+    fecha_publicacion = models.DateField(auto_now_add=True)
+    activa = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.titulo
+
+class Postulacion(models.Model):
+    ESTADOS = [('Enviada', 'Enviada'), ('En Revisión', 'En Revisión'), ('Aceptada', 'Aceptada'), ('Rechazada', 'Rechazada')]
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    oferta = models.ForeignKey(OfertaLaboral, on_delete=models.CASCADE)
+    fecha_postulacion = models.DateTimeField(auto_now_add=True)
+    estado = models.CharField(max_length=30, choices=ESTADOS, default='Enviada')
+
+    class Meta:
+        unique_together = ('estudiante', 'oferta') # Primary key compuesta
+
+# --- 3. RUTAS DE APRENDIZAJE ---
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=200)
+    lenguaje = models.CharField(max_length=100)
+    es_ingles_tecnico = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nombre
+
+class Leccion(models.Model):
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    orden = models.PositiveIntegerField()
+    titulo = models.CharField(max_length=200)
+    completada = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('curso', 'orden')
+
+    def __str__(self):
+        return f"{self.orden}. {self.titulo} ({self.curso.nombre})"
+
+class Ejercicio(models.Model):
+    TIPOS = [('C', 'Código'), ('Q', 'Quiz')]
+    leccion = models.ForeignKey(Leccion, on_delete=models.CASCADE)
+    tipo_ejercicio = models.CharField(max_length=1, choices=TIPOS)
+    enunciado = models.TextField()
+    xp_recompensa = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"Ejercicio {self.id} - {self.get_tipo_ejercicio_display()}"
+
+class RetoCodigo(models.Model):
+    ejercicio = models.OneToOneField(Ejercicio, on_delete=models.CASCADE, primary_key=True)
+    codigo_base = models.TextField()
+    casos_prueba = models.JSONField()
+    tiempo_limite = models.FloatField()
+
+class QuizIngles(models.Model):
+    ejercicio = models.OneToOneField(Ejercicio, on_delete=models.CASCADE, primary_key=True)
+    url_audio = models.URLField(max_length=500, blank=True, null=True)
+    opciones = models.JSONField()
+    respuesta_correcta = models.PositiveIntegerField()
+
+class ProgresoCurso(models.Model):
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    porcentaje_completado = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+    nivel_alcanzado = models.PositiveIntegerField(default=0)
+    ultimo_acceso = models.DateField(auto_now=True)
+
+    class Meta:
+        unique_together = ('estudiante', 'curso')
+
+# --- 4. GAMIFICACIÓN Y FORO ---
+
+class LigaSemanal(models.Model):
+    division = models.CharField(max_length=50)
+    fecha_cierre = models.DateField()
+
+    def __str__(self):
+        return self.division
+
+class RankingSemanal(models.Model):
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    liga = models.ForeignKey(LigaSemanal, on_delete=models.CASCADE)
+    semana_inicio = models.DateField()
+    xp_ganada_esta_semana = models.PositiveIntegerField(default=0)
+    puesto_actual = models.PositiveIntegerField(blank=True, null=True)
+
+    class Meta:
+        unique_together = ('estudiante', 'semana_inicio')
+
+class DiscusionForo(models.Model):
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=200)
+    es_duda_tecnica = models.BooleanField(default=False)
+    votos = models.IntegerField(default=0)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    # Se usa 'self' en cadena porque RespuestaForo aún no está definida en el código
+    solucion_respuesta = models.OneToOneField('RespuestaForo', on_delete=models.SET_NULL, null=True, blank=True, related_name='discusion_solucionada')
+
+    def __str__(self):
+        return self.titulo
+
+class RespuestaForo(models.Model):
+    discusion = models.ForeignKey(DiscusionForo, on_delete=models.CASCADE, related_name='respuestas')
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
+    contenido = models.TextField()
+    fecha_respuesta = models.DateTimeField(auto_now_add=True)
+    votos = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Respuesta de {self.estudiante.usuario.email}"
+
+class Perfil(models.Model):
+    # OneToOneField significa que cada Usuario tendrá exactamente UN Perfil (una mochila)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    # Aquí guardaremos los puntos. Empezarán en 0.
+    xp_total = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Perfil de {self.usuario.username} - {self.xp_total} XP"
---
+++ aprendizaje/views.py
@@ -0,0 +1,90 @@
+import sys
+import io
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
+from .models import Curso, Leccion, Ejercicio, Estudiante
+
+def lista_cursos(request):
+    # Obtenemos todos los cursos
+    cursos = Curso.objects.all() 
+    return render(request, 'aprendizaje/lista_cursos.html', {'cursos': cursos})
+
+def detalle_curso(request, curso_id):
+    # ¡Aquí usamos la herramienta que importamos en la línea 1!
+    curso = get_object_or_404(Curso, id=curso_id)  
+    # Buscamos las lecciones de este curso específico
+    lecciones = Leccion.objects.filter(curso=curso).order_by('orden')
+    return render(request, 'aprendizaje/detalle_curso.html', {
+        'curso': curso, 
+        'lecciones': lecciones
+    })
+
+def detalle_leccion(request, leccion_id):
+    leccion = get_object_or_404(Leccion, id=leccion_id)
+    ejercicios = Ejercicio.objects.filter(leccion=leccion)
+    
+    # Variables para enviar a la plantilla
+    mensaje = None
+    es_correcto = False
+    codigo_previo = ""
+
+    if request.method == 'POST':
+        codigo_recibido = request.POST.get('codigo_alumno')
+        codigo_previo = codigo_recibido # Guardamos lo que escribió para que no se le borre
+        
+        # Preparamos una "trampa" para capturar lo que el código imprima
+        salida_capturada = io.StringIO()
+        salida_original = sys.stdout
+        sys.stdout = salida_capturada
+        
+        try:
+            # ¡Aquí ocurre la magia! Ejecutamos el texto como si fuera código Python
+            exec(codigo_recibido)
+            
+            # Si Python pasa la línea anterior sin explotar, significa que no hubo errores de sintaxis
+            salida_texto = salida_capturada.getvalue()
+            
+            # Puedes personalizar este mensaje
+            mensaje = f"¡Excelente! Tu código funcionó sin errores. Resultado impreso: {salida_texto}"
+            es_correcto = True
+
+            leccion.completada = True
+            leccion.save()
+            
+        except Exception as e:
+            # Si el código del alumno tiene un error, entramos aquí
+            mensaje = f"Ups, encontramos un error: {e}"
+            es_correcto = False
+            
+        finally:
+            # MUY IMPORTANTE: Devolvemos la salida a la normalidad
+            sys.stdout = salida_original
+
+    return render(request, 'aprendizaje/detalle_leccion.html', {
+        'leccion': leccion,
+        'ejercicios': ejercicios,
+        'mensaje': mensaje,
+        'es_correcto': es_correcto,
+        'codigo_previo': codigo_previo # Enviamos el código de vuelta
+    })
+
+def registro(request):
+    if request.method == 'POST':
+        # Volvemos a usar el formulario original de Django
+        form = UserCreationForm(request.POST) 
+        if form.is_valid():
+            # Esto guarda al User de Django (con su contraseña segura)
+            user_django = form.save()
+            
+            # Creamos la mochila del Estudiante conectada a ese User
+            # Usamos xp_total=0 porque así se llama en tu modelo
+            Estudiante.objects.create(usuario=user_django, xp_total=0, racha_dias=0)
+            
+            login(request, user_django)
+            return redirect('lista_cursos')
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'aprendizaje/registro.html', {'form': form})
